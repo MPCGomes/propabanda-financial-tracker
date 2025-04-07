e21fb644f085a45cfe6cab70f6bd846f757d0b6e
Maria Júlia Miosso <107309474+mjuliamiosso@users.noreply.github.com>
2025-04-07 15:49:38 -0300

    fix

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,11 +1,12 @@
+
 type HomePageProps = {
   title: string;
 };
 
 export default function HomePage({ title }: HomePageProps) {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-3xl font-bold text-gray-800">{title}</h1>
+    <div className="">
+      
     </div>
   );
 }
