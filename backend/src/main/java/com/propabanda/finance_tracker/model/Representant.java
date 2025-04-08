dcaf18151f06988e70e8172e281cac48d8c1a2ef
MPCGomes <mpcgomes2@gmail.com>
2025-04-08 15:39:29 -0300

    add representant

---
+++ backend/src/main/java/com/propabanda/finance_tracker/model/Representant.java
@@ -0,0 +1,31 @@
+package com.propabanda.finance_tracker.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Entity
+@Table(name = "representant")
+@Getter
+@Setter
+public class Representant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 100)
+    private String name;
+
+    @NotBlank
+    @Email
+    @Size(max = 100)
+    private String email;
+
+    @NotBlank
+    @Size(min = 10, max = 11)
+    private String phone;
+}
