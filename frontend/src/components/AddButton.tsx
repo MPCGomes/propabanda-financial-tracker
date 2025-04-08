cf2934422b81d27bac5097df6eca377d6a7be10b
Maria Júlia Miosso <107309474+mjuliamiosso@users.noreply.github.com>
2025-04-08 16:03:13 -0300

    refactor: floating buttons

--- frontend/src/components/AddButton.tsx
+++
@@ -1,9 +0,0 @@
-import { FaPlus } from "react-icons/fa6";
-
-export default function FloatingButton() {
-  return (
-    <button className="w-10 h-10 text-xl text-white bg-[#FFA322] flex items-center justify-center rounded-full cursor-pointer absolute bottom-5 right-5">
-      <FaPlus />
-    </button>
-  );
-}
