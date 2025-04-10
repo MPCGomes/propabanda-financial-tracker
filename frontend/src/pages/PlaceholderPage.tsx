ac85c6e791a72a587fc06e3e6c85f63c84616dba
Maria Júlia Miosso <107309474+mjuliamiosso@users.noreply.github.com>
2025-04-10 15:28:02 -0300

    update placeholder

--- frontend/src/pages/PlaceholderPage.tsx
+++ frontend/src/pages/PlaceholderPage.tsx
@@ -1,3 +1,4 @@
+import Header from "../components/Header";
 import PlaceholderBox from "../components/PlaceholderComponent";
 
 type PlaceholderPageProps = {
@@ -6,9 +7,23 @@ type PlaceholderPageProps = {
 
 export default function PlaceholderPage({ title }: PlaceholderPageProps) {
   return (
-    <div className="min-h-screen flex flex-col gap-4 items-center justify-center bg-gray-100">
-      <h1 className="text-3xl font-bold text-gray-800">{title}</h1>
-      <PlaceholderBox text={`This is the ${title.toLowerCase()}.`} />
-    </div>
+    <section className="bg-[#f6f6f6] lg:flex justify-center items-center min-h-screen lg:p-3">
+          <div className="w-full max-w-[1280px] flex lg:flex-row gap-5">
+            {/* Header */}
+            <div
+              className="fixed bottom-0 w-full bg-white rounded-lg 
+            flex justify-center p-1
+            lg:static lg:w-40 lg:flex lg:flex-col lg:justify-start lg:p-2"
+            >
+              <Header clients="active" />
+            </div>
+    
+            {/* Content */}
+            <div className="flex flex-col gap-5 w-full p-4 pb-[100px] lg:p-0 lg:pb-0">
+             
+              
+            </div>
+          </div>
+        </section>
   );
 }
