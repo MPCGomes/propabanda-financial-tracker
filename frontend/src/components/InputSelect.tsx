d62fe18485cdb25bc418de9bc251ad200942b866
Maria Júlia Miosso <107309474+mjuliamiosso@users.noreply.github.com>
2025-04-11 15:24:59 -0300

    feat: select input

---
+++ frontend/src/components/InputSelect.tsx
@@ -0,0 +1,38 @@
+import { MdKeyboardArrowDown } from "react-icons/md";
+
+type Option = {
+  value: string;
+  label: string;
+};
+
+type SelectInputProps = {
+  label: string;
+  id: string;
+  options: Option[];
+};
+
+export default function SelectInput({ label, id, options }: SelectInputProps) {
+  return (
+    <div className="relative w-full">
+      <select
+        id={id}
+        defaultValue=""
+        className="peer w-full border border-gray-300 rounded-md p-2 pt-7 pr-8 text-sm text-gray-700 bg-white appearance-none focus:outline-none focus:border-blue-500"
+      >
+        <option value="" disabled hidden></option>
+        {options.map((opt) => (
+          <option key={opt.value} value={opt.value}>
+            {opt.label}
+          </option>
+        ))}
+      </select>
+      <label
+        htmlFor={id}
+        className="absolute left-2 top-2 text-[#282828] text-sm transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-sm peer-placeholder-shown:text-[#282828] peer-focus:top-2 peer-focus:text-xs peer-focus:text-blue-500"
+      >
+        {label}
+      </label>
+      <MdKeyboardArrowDown className="pointer-events-none absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-500 text-xl" />
+    </div>
+  );
+}
--- frontend/src/components/InputText.tsx
+++ frontend/src/components/InputText.tsx
@@ -1,23 +1,20 @@
-type PlaceholderComponent = {
+type InputText = {
   label: string;
   placeholder: string;
 };
 
-export default function PlaceholderComponent({
-  label,
-  placeholder,
-}: PlaceholderComponent) {
+export default function InputText({ label, placeholder }: InputText) {
   return (
     <div className="relative w-full">
       <input
         type="text"
         id="meuInput"
         placeholder={placeholder}
-        className="peer w-full border border-gray-300 rounded-md p-2 pt-5 text-sm placeholder-transparent focus:outline-none focus:border-blue-500"
+        className="peer w-full border tex-base border-gray-300 rounded-md p-2 pt-7 text-sm focus:outline-none focus:border-blue-500 text-[#282828]"
       />
       <label
         htmlFor="meuInput"
-        className="absolute left-2 top-2 text-gray-500 text-xs transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-sm peer-placeholder-shown:text-gray-400 peer-focus:top-2 peer-focus:text-xs peer-focus:text-blue-500"
+        className="absolute left-2 top-2 text-[#282828] text-xs transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-sm peer-placeholder-shown:text-[#282828] peer-focus:top-2 peer-focus:text-xs peer-focus:text-blue-500"
       >
         {label}
       </label>
