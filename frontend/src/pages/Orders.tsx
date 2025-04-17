40e2eb9ed828a86f9178ea2d0cd19130c551bd41
Maria Júlia Miosso <107309474+mjuliamiosso@users.noreply.github.com>
2025-04-17 14:29:44 -0300

    feat: modal

--- frontend/src/pages/Orders.tsx
+++ frontend/src/pages/Orders.tsx
@@ -5,12 +5,19 @@ import { FaPlus } from "react-icons/fa6";
 import FloatingButton from "../components/FloatingButton";
 import Filter from "../components/Filter";
 import SearchBar from "../components/SearchBar";
+import Modal from "../components/Modal";
+import { useState } from "react";
+import Button from "../components/Button";
 
 type PlaceholderPageProps = {
   title: string;
 };
 
 export default function PlaceholderPage({ title }: PlaceholderPageProps) {
+  const [openModal, setOpenModal] = useState<
+    null | "order" | "date" | "status" | "item"
+  >(null);
+
   return (
     <section className="bg-[#f6f6f6] lg:flex justify-center items-center min-h-screen lg:p-3 lg:items-start">
       <div className="w-full max-w-[1280px] flex lg:flex-row gap-5 pt-12 lg:pt-20 lg:pb-22">
@@ -28,13 +35,119 @@ export default function PlaceholderPage({ title }: PlaceholderPageProps) {
           <div className="flex gap-5 flex-col lg:flex-row lg:justify-between lg:items-center">
             <SearchBar />
 
+            {/* Filters */}
             <div className="flex gap-3 flex-wrap lg:flex-nowrap">
-              <Filter text={"Ordem"} />
-              <Filter text={"Data"} />
-              <Filter text={"Status"} />
-              <Filter text={"Item"} />
+              <Filter text={"Ordem"} onClick={() => setOpenModal("order")} />
+              <Filter text={"Data"} onClick={() => setOpenModal("date")} />
+              <Filter text={"Status"} onClick={() => setOpenModal("status")} />
+              <Filter text={"Item"} onClick={() => setOpenModal("item")} />
             </div>
           </div>
+
+          {/* Modal: Alphabetical Order */}
+          <Modal
+            isOpen={openModal === "order"}
+            onClose={() => setOpenModal(null)}
+            title="Ordem Alfabética"
+          >
+            <div className="flex flex-col gap-2">
+              <label htmlFor="a-z">
+                <input type="radio" id="a-z" name="order" /> Ascendente (A-Z)
+              </label>
+              <label htmlFor="z-a">
+                <input type="radio" id="z-a" name="order" /> Descentente (Z-A)
+              </label>
+            </div>
+            <Button text={"Aplicar filtro"} />
+          </Modal>
+
+          {/* Modal: Date */}
+          <Modal
+            isOpen={openModal === "date"}
+            onClose={() => setOpenModal(null)}
+            title="Período"
+          >
+            <div className="flex gap-2">
+              <div className="relative w-full">
+                <input
+                  type="date"
+                  id="start-date"
+                  className="peer w-full border border-gray-300 rounded-md p-2 pt-7 text-sm text-gray-700 bg-white appearance-none focus:outline-none focus:border-blue-500s"
+                  placeholder=" "
+                />
+                <label
+                  htmlFor="start-date"
+                  className="absolute left-2 top-2 text-[#282828] text-xs transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-xs peer-placeholder-shown:text-[#282828] peer-focus:top-2 peer-focus:text-xs peer-focus:text-blue-500"
+                >
+                  Início
+                </label>
+              </div>
+              <div className="relative w-full">
+                <input
+                  type="date"
+                  id="start-date"
+                  className="peer w-full border border-gray-300 rounded-md p-2 pt-7 text-sm text-gray-700 bg-white appearance-none focus:outline-none focus:border-blue-500s"
+                  placeholder=" "
+                />
+                <label
+                  htmlFor="start-date"
+                  className="absolute left-2 top-2 text-[#282828] text-xs transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-xs peer-placeholder-shown:text-[#282828] peer-focus:top-2 peer-focus:text-xs peer-focus:text-blue-500"
+                >
+                  Início
+                </label>
+              </div>
+            </div>
+
+            <Button text={"Aplicar filtro"} />
+          </Modal>
+
+          {/* Modal: Status */}
+          <Modal
+            isOpen={openModal === "status"}
+            onClose={() => setOpenModal(null)}
+            title="Status"
+          >
+            <div className="flex flex-col gap-2">
+              <label htmlFor="paid">
+                <input type="radio" id="paid" name="order" /> Pago
+              </label>
+              <label htmlFor="pending">
+                <input type="radio" id="pending" name="order" /> Pendente
+              </label>
+              <label htmlFor="unpaid">
+                <input type="radio" id="unpaid" name="order" /> Não pago
+              </label>
+            </div>
+            <Button text={"Aplicar filtro"} />
+          </Modal>
+
+          {/* Modal: Item */}
+          <Modal
+            isOpen={openModal === "item"}
+            onClose={() => setOpenModal(null)}
+            title="Item"
+          >
+            <div className="flex flex-col gap-2">
+              <label>
+                <input type="checkbox" /> OUTDOOR 1
+              </label>
+              <label>
+                <input type="checkbox" /> OUTDOOR 2
+              </label>
+              <label>
+                <input type="checkbox" /> PAINEL
+              </label>
+              <label>
+                <input type="checkbox" /> REDES SOCIAIS
+              </label>
+              <label>
+                <input type="checkbox" /> SITES
+              </label>
+            </div>
+            <Button text={"Aplicar filtro"} />
+          </Modal>
+
+          {/* Orders */}
           <div className="flex flex-col gap-3 p-2 bg-white rounded-lg">
             <Order
               product={"Produto"}
@@ -95,6 +208,8 @@ export default function PlaceholderPage({ title }: PlaceholderPageProps) {
           </div>
         </div>
       </div>
+
+      {/* Floating Button */}
       <a
         href="/order/register"
         className="fixed bottom-25 right-4 lg:bottom-10 lg:right-5"
