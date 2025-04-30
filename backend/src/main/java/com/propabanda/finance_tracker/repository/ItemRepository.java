ef5dbba5b24665c56ff958d3f065df2388d4bb30
MPCGomes <150841644+MPCGomes@users.noreply.github.com>
2025-04-30 16:37:19 -0300

    add excel data import

--- backend/src/main/java/com/propabanda/finance_tracker/repository/ItemRepository.java
+++ backend/src/main/java/com/propabanda/finance_tracker/repository/ItemRepository.java
@@ -3,6 +3,10 @@ package com.propabanda.finance_tracker.repository;
 import com.propabanda.finance_tracker.model.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
     boolean existsByName(String name);
+
+    Optional<Item> findByNameIgnoreCase(String name);
 }
