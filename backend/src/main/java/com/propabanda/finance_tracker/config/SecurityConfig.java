d3fa2fbce51e851ff1990337be07c817041e03e0
MPCGomes <150841644+MPCGomes@users.noreply.github.com>
2025-05-08 17:41:50 -0300

    fix cors

--- backend/src/main/java/com/propabanda/finance_tracker/config/SecurityConfig.java
+++ backend/src/main/java/com/propabanda/finance_tracker/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.propabanda.finance_tracker.config;
 import com.propabanda.finance_tracker.Security.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -21,6 +22,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
