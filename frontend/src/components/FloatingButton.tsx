5575d9f0406158dbe4b03b3231d7cf6d3bfc30b3
MPCGomes <150841644+MPCGomes@users.noreply.github.com>
2025-05-15 19:39:13 -0300

    fix floating button

--- frontend/src/components/FloatingButton.tsx
+++ frontend/src/components/FloatingButton.tsx
@@ -1,12 +1,18 @@
-type FloatingButton = {
+type FloatingButtonProps = {
   icon: React.ReactNode;
   background: string;
+  onClick?: () => void;
 };
 
-export default function FloatingButton({ icon, background }: FloatingButton) {
+export default function FloatingButton({
+  icon,
+  background,
+  onClick,
+}: FloatingButtonProps) {
   return (
     <button
-      style={{background}}
+      style={{ background }}
+      onClick={onClick}
       className="w-10 h-10 text-xl text-white flex items-center justify-center rounded-full cursor-pointer"
     >
       {icon}
