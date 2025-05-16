660cba57cf31c99024cfb50722390260c8b1abcc
MPCGomes <150841644+MPCGomes@users.noreply.github.com>
2025-05-16 16:52:31 -0300

    add items migration

---
+++ backend/src/main/resources/db/migration/V2__Items.sql
@@ -0,0 +1,25 @@
+TRUNCATE TABLE order_item_link;
+TRUNCATE TABLE order_tb;
+TRUNCATE TABLE item CASCADE;
+
+INSERT INTO item (name) VALUES
+('Painel 01'),
+('Painel 02'),
+('Painel 03'),
+('Painel 04'),
+('Painel 05'),
+('Painel 06'),
+('Painel 07'),
+('Painel 08'),
+('Outdoor 01'),
+('Outdoor 02'),
+('Outdoor 03'),
+('Outdoor 04'),
+('Outdoor 05'),
+('Outdoor 06'),
+('Outdoor 07'),
+('Outdoor 08'),
+('Marketing Digital'),
+('Site'),
+('Arte Gráfica'),
+('Outros');
